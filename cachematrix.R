f2e9961c9f65fa77d185a8e254e208e8bb017276
SCookson <sj_cookson@yahoo.co.uk>
2015-02-22 12:41:07 +0000

    explained more about setting m to be null

--- cachematrix.R
+++ cachematrix.R
@@ -14,7 +14,8 @@ makeCacheMatrix <- function(x = matrix()) {
       m <- NULL  		## 'm' is an empty matrix within the makeCacheMatrix environment
       set <- function(y) {	## i. Function recreating original matrix
       x <<- y  		  ## Deep assignment: so inputted matrix 'x' can be recalled outside of this environment 
-      m <<- NULL		## Deep assignment: so 'm' is still an empty vector outside this environment
+      m <<- NULL		## Deep assignment: reset 'm' to be an empty matrix outside this environment
+                    ## So later on we don't call the wrong matrix from the cache - could happen if we run these functions on multiple matrices
       }
       get <- function() x	        ## ii. Function returning matrix 'x'
       setinverse <- function(solve) m <<- solve	## iii. Function calculating the inverted matrix
@@ -30,11 +31,13 @@ cacheSolve <- function(x, ...) {
       m <- x$getinverse()   ## define matrix 'm' to be the cached inverted matrix 
       if(!is.null(m)) {     ## check for the inverted matrix in the cache
       ## if it's a cache hit ...
+      ## (because we have already run this function once & haven't changed matrix)
       ## this code will run quickly
       message("getting cached data")  ## let the user know we are retrieving from the cache
       return(m)             ## return the inverted matrix from the cache
       }                           
       ## if it's a cache miss ...
+      ## (because it's the 1st time we have run this function on this matrix OR we changed matrices at some point)
       ## this code may take a some time to run if the matrix is large
       data <- x$get()       ## get function retrieves the original matrix 'x'
       m <- solve(data, ...) ## invert the retrieved matrix
